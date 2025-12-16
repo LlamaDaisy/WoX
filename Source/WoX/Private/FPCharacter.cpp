0e26b2245b0f2731c2b6aa4d6e1adbe3e4ce80cf
LlamaDaisy <laralowndes@googlemail.com>
2025-12-16 19:32:55 +0000

    Adjusted Torch Source, Added prompt for players, made it so crouching does not trigger pressure plates.

--- Source/WoX/Private/FPCharacter.cpp
+++ Source/WoX/Private/FPCharacter.cpp
@@ -19,7 +19,7 @@ AFPCharacter::AFPCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	bUseControllerRotationPitch = true;
+	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
 
